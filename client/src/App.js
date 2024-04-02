71f6feb20bfa2f8cd7b82256ed71bdff5fecfecc
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2024-04-01 17:31:21 -0700

    docker compose

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,6 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
